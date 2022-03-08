30d634d00bc2b038572c5c4a83ba1010df95aa0c
Renan <rnsouza.ti@gmail.com>
2022-03-08 16:58:00 -0400

    Concatenando e interpolando variáveis

---
+++ Javascript/Variaveis/concatenacao.js
@@ -0,0 +1,33 @@
+// Or declaration
+
+var name
+
+
+// Assigment or atribuição de valores
+
+name = "Fulano"
+
+
+//que tipo de dado foi colocado na variavel
+
+console.log(typeof name)
+
+
+let age = 28
+let isHuman = true
+
+//or 
+
+let ages, human
+ages = 30
+human = false
+
+console.log(name, age, ages, isHuman, human)
+
+//Escrita do testo + variaveis
+console.log('O ' + name + ' tem ' + age + ' anos de idade.')
+
+
+//Interpolando valores com template literals or template strings
+
+console.log('O ${name} tem ${age} anos.')
