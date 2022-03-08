e5bfde5b251391a958e96006b746956cac74b457
Renan <rnsouza.ti@gmail.com>
2022-03-08 17:06:35 -0400

    Objects em JavaScript

---
+++ Javascript/Variaveis/object.js
@@ -0,0 +1,12 @@
+const person ={
+    name: 'Fulano',
+    age: 39,
+    weight: 99,
+    isAdmin: false
+
+}
+
+console.log(person.age)
+console.log(person.name)
+console.log(person.isAdmin)
+console.log(person)
--- Javascript/scripts.js
+++ Javascript/scripts.js
@@ -1,33 +1,10 @@
-// Or declaration
+const person = {
+    name: 'Fulano',
+    age: 33,
+    weight: 90.1,
+    isAdmin: true
+}
 
-var name
-
-
-// Assigment or atribuição de valores
-
-name = "Fulano"
-
-
-//que tipo de dado foi colocado na variavel
-
-console.log(typeof name)
-
-
-let age = 28
-let isHuman = true
-
-//or 
-
-let ages, human
-ages = 30
-human = false
-
-console.log(name, age, ages, isHuman, human)
-
-//Escrita do testo + variaveis
-console.log('O ' + name + ' tem ' + age + ' anos de idade.')
-
-
-//Interpolando valores com template literals or template strings
-
-console.log('O ${name} tem ${age} anos.')
+console.log(person.age)
+console.log(person.name)
+console.log(person)
