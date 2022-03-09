10dead980b9f42603969a4ac26b04598a555295f
Renan <rnsouza.ti@gmail.com>
2022-03-09 14:32:27 -0400

    Retornando valores dentro da função em JavaScript 2° explicaçao

--- Javascript/Functions/functions_1.js
+++ Javascript/Functions/functions_1.js
@@ -17,3 +17,12 @@ let number2 = 33
 console.log('SOMA '+ soma(2,33.3))
 console.log('SOMA: '+ soma(number1, number2))
 //console.log(total)//Nao recomendado
+
+
+function fazerSuco(fruta1, fruta2){
+    return 'Suco de ' + fruta1+fruta2
+}
+
+const copo = fazerSuco('banana', 'laranja')
+
+console.log(copo)
