7973b81fa774a9fd1a27e5218066392e90357a2c
Renan <rnsouza.ti@gmail.com>
2022-03-09 11:53:13 -0400

    Funçoes  em JavaScript

---
+++ Javascript/Functions/functions.js
@@ -0,0 +1,12 @@
+console.log('Sobre Function')
+
+//APP de frases motivacionais
+
+//declaration function
+function createPhrases(){
+    console.log('Estudar é muito bom')
+    console.log('Paciencia e persistencia')
+    console.log('Revisao é mae do aprendizado')
+}
+
+createPhrases()
