70831fde367ac08f2b5d07b2ecf99fde8cc435fb
Renan <rnsouza.ti@gmail.com>
2022-03-09 14:47:18 -0400

    Function explicaçao

---
+++ Javascript/Functions/functionScope.js
@@ -0,0 +1,11 @@
+let subject = 'Pensando em algo ...'
+
+
+function createThink(subject) {
+    subject = 'Outro pensamento'
+    return subject
+}
+
+
+console.log(createThink(subject))
+ console.log(subject)
