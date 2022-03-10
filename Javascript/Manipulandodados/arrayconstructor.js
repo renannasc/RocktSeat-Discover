efd89967b6738764dd6d16f6aa3347a50cd6b2ba
Renan <rnsouza.ti@gmail.com>
2022-03-10 16:33:13 -0400

    Construtor de arrays  em JavaScript

---
+++ Javascript/Manipulandodados/arrayconstructor.js
@@ -0,0 +1,10 @@
+// let myArray = ['a','b', 'c']
+// console.log(myArray)
+
+//or
+
+let myArrayResult = new Array('d', 'e', 'f')
+console.log(myArrayResult)
+
+let myArrayPosition = new Array(10)
+console.log(myArrayPosition)
