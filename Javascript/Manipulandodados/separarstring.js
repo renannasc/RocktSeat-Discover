d32e3a8041084da5f8a406b3c8e4e250ccff7b78
Renan <rnsouza.ti@gmail.com>
2022-03-10 16:15:39 -0400

    Separaçao de frase e array em JavaScript

---
+++ Javascript/Manipulandodados/separarstring.js
@@ -0,0 +1,8 @@
+let phrase = "Eu quero viver o amor"
+let myArray = phrase.split(" ")
+
+console.log(myArray)
+
+
+let phraseWithUnderscore = myArray.join("_")
+console.log(phraseWithUnderscore.toLocaleUpperCase())
