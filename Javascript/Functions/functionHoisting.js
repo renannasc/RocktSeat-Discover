0cfb40f40eadb2721d63fdd276de0a5606bc50c3
Renan <rnsouza.ti@gmail.com>
2022-03-10 09:28:59 -0400

    Hosting de funçoes em Javascript

---
+++ Javascript/Functions/functionHoisting.js
@@ -0,0 +1,19 @@
+sayMyName()
+
+
+function sayMyName(){
+    console.log('Renan')//SOFRE ELEVAÇÃO OU SEJA HOSTING
+}
+
+
+// const sayMyName = function(){
+//     console.log('Renan')//NÃO SOFRE ELEVAÇÃO OU SEJA HOSTING
+// }
+
+// var sayMyName = function(){
+//     console.log('Renan')//NÃO SOFRE ELEVAÇÃO OU SEJA HOSTING
+// }
+
+// let sayMyName = function(){
+//     console.log('Renan')//NÃO SOFRE ELEVAÇÃO OU SEJA HOSTING
+// }
