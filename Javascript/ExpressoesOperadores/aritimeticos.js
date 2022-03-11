b29f279678107f04241dc641e7045dbc1065428e
Renan <rnsouza.ti@gmail.com>
2022-03-10 21:40:59 -0400

    Operadores aritimeticos em JavaScript

---
+++ Javascript/ExpressoesOperadores/aritimeticos.js
@@ -0,0 +1,43 @@
+/**
+ * Operadores aritimeticos
+ * 
+ * Multiplicaçao
+ * divisao
+ * soma
+ * subtração
+ * 
+ * Resto da divisão
+ * Incremento
+ * Decremento
+ * Exponencial
+ * 
+ */
+
+
+console.log(2 * 2)
+console.log(2 / 2)
+console.log(2 + 2)
+console.log(2 - 2)
+
+//RESTO DA DIVISAO
+let remainder
+remainder = 2 % 2 
+console.log(remainder)
+
+//INCREMENTANDO UM NUMERO A VARIAVEL
+let increment = 0
+increment++
+console.log(increment)
+
+//DECREMENTRANDO UM NUMERO A VARIAVEL
+let decrement = 0
+decrement--
+console.log(decrement)
+
+//EXPONENCIAÇÃO
+console.log(2 ** 2)
+
+
+// console.log(2 % 2)
+// console.log(++2)
+// console.log(--2)
