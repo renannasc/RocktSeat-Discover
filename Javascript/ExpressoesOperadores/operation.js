017ba30ca65c399d9667b1cdd166c372ea3e22e6
Renan <rnsouza.ti@gmail.com>
2022-03-10 23:19:33 -0400

    Operadores de comparaçao em JS

---
+++ Javascript/ExpressoesOperadores/operation.js
@@ -0,0 +1,14 @@
+//Compara valores e retornar um boolean como resposta a comparação
+
+
+let one = 1
+let two = 2
+
+//  == IGUAL
+console.log(two == 1)
+console.log(one == "1")
+
+//  != DIFERENTE
+console.log(one != two)
+console.log(one != 1)
+console.log(one != "1")
