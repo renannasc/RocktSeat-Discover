54bf5adae85cb62212b88618f87fa931e4c4a4bb
Renan <rnsouza.ti@gmail.com>
2022-03-10 21:24:26 -0400

    New para novos objetos em JavaScript

---
+++ Javascript/ExpressoesOperadores/new.js
@@ -0,0 +1,11 @@
+// NEW SERVE PARA CRIAR UM OBJETO
+
+
+let name = new String('Renan')
+name.lastName = 'Nascimento'
+let age = new Number('28')
+console.log(name, name.lastName, age)
+
+
+let date = new Date('2020-03-10')
+console.log(date.__proto__)//COM O PROTO POSSO VERIFICAR ALGUMAS DAS PROPRIEDADES DO METODO DATE
