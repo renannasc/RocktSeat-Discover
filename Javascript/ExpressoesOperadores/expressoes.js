76b8c01b0f487506d79a735b017c3225d1da7a55
Renan <rnsouza.ti@gmail.com>
2022-03-10 21:18:39 -0400

    Expressoes em JavaScript

---
+++ Javascript/ExpressoesOperadores/expressoes.js
@@ -0,0 +1,17 @@
+// let number = 1;
+
+// console.log(number)
+
+//EXPRESSAO DE FUNCAO AUTO EXECUTAVEL
+(function(){
+    console.log("Alo Mundo")
+})()
+
+let number = 1
+console.log(number + 1)
+console.log(++number)
+console.log(--number)
+console.log(typeof number)
+
+
+console.log(true ? 'alo' : 'nada') //SE FOR VERDAEDIRO MOSTRA ALO SE NAO FOR MOSTRE NADA
