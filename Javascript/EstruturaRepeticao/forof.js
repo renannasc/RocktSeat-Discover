cf45b0591a4563f92fd34aca282876f5256a9b4c
Renan <rnsouza.ti@gmail.com>
2022-03-13 13:09:43 -0400

    For...of em JS

---
+++ Javascript/EstruturaRepeticao/forof.js
@@ -0,0 +1,13 @@
+let name = 'Renan'
+let names = ['Renan', 'Nina', 'Fulano']
+
+console.log('For...of com strings')
+for (const iterator of name) {
+    console.log(iterator)
+}
+
+
+console.log('For...of com Arrays')
+for (let operator of names) {
+    console.log(operator)
+}
