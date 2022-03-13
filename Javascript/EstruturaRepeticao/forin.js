3666be1258f3b148df5ee47512e60ea68409441e
Renan <rnsouza.ti@gmail.com>
2022-03-13 13:15:34 -0400

    For...in em JS

---
+++ Javascript/EstruturaRepeticao/forin.js
@@ -0,0 +1,14 @@
+
+
+let person = {
+    name: 'Fulano',
+    age: 33,
+    weight: 99.8
+
+}
+
+for (let key in person) {
+    console.log(key)
+    
+}
+//console.log(person.name, person.age, person.weight)
